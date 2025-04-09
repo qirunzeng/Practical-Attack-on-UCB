7a7e3906f0e6d383bced0da7eab2d9e801daa3af
qirunzeng <qirunzeng@gmail.com>
2025-04-09 13:04:22 +0800

    fix bugs

--- include/atk.h
+++ include/atk.h
@@ -14,7 +14,7 @@ namespace atk {
     double e = 2.71828;
     double cost = 0.00;
 
-    void run(int K, int T, double delta = 0.1) {
+    void run(int K, int T, double delta, int R) {
         env::init(K, sigma);
         std::cout << "Init over" << std::endl;
         bandit::ucb bandit_alg(K, sigma);
@@ -27,16 +27,27 @@ namespace atk {
             return sqrt(2 * sigma * sigma * log(pi * K * n * n / e / delta) / n);
         };
 
+        bool flag = true;
         while (bandit_alg.time_slot < T) {
             int arm = bandit_alg.step();
+
+            if (bandit_alg.time_slot % (R+1) == 0) {
+                flag = true;
+            }
+
             if (arm != target) {
                 // non_target_pull++;
                 double b = beta(bandit_alg.pull[target]);
                 // std::cout << b << std::endl;
                 double fdbk0 = env::generateGaussian(arm);
-                double fdbk = (bandit_alg.empirical_mean_vec[target]-2*b-3*sigma) * (bandit_alg.pull[arm]+1) - bandit_alg.empirical_mean_vec[arm] * bandit_alg.pull[arm];
-                bandit_alg.update(fdbk);
-                cost += fdbk0 - fdbk;
+                if (flag) {
+                    double fdbk = (bandit_alg.empirical_mean_vec[target]-2*b-3*sigma) * (bandit_alg.pull[arm]+1) - bandit_alg.empirical_mean_vec[arm] * bandit_alg.pull[arm];
+                    bandit_alg.update(fdbk);
+                    cost += fdbk0 - fdbk;
+                    flag = false;
+                } else {
+                    bandit_alg.update(fdbk0);
+                }
                 // std::cout << "(" << fdbk << " " << fdbk0 << ") ";
             } else {
                 bandit_alg.update(env::generateGaussian(arm));
--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include "atk.h"
 
 int main() {
-    atk::run(16, (int)1000000000, 0.1);
+    atk::run(16, (int)1000000000, 0.1, 100);
     return 0;
 }
